38f62f01f140b65863164818030ab84ab61a7f1c
Joe Sandmeyer <github.com@unconscious.com>
2023-06-21 18:23:29 -0400

    hide claimNFT

--- src/pages/NftDetail.tsx
+++ src/pages/NftDetail.tsx
@@ -3,33 +3,6 @@ import { useExperience, useBusiness, useUser } from "@vatom/experience-sdk";
 import './Common.css'
 
 
-const claimNFT = async () => {
-	const objectDefinitionId = 'RYHEO817ml'
-  const { navigateToWallet, navigateToNFTDetail } = useExperience();
-  const { business, tokens } = useBusiness();
-	const user = useUser();
-	const tokenToID = user.accessToken
-	const found = tokens.find(t => t.studioInfo?.objectDefinitionId === objectDefinitionId)
-
-	if (found) {
-		navigateToNFTDetail(found.tokenId, business?.id ?? '')
-	} else {
-		try {
-			const response = await fetch(
-				'https://distribution.api.vatominc.com/me/get-flna/ewogICAgImNhbXBhaWduSWQiOiAiOWxleHJDRmFHdiIsCiAgICAib2JqZWN0RGVmaW5pdGlvbklkIjogIktLdTlNbFBWMFEiCn0=',
-				{
-					method: 'GET',
-					headers: { Authorization: `Bearer ${tokenToID}` }
-				}
-			)
-			navigateToWallet()
-		} catch (err) {
-			console.error(err)
-		}
-	}
-}// claimNFT()
-
-
 const NftDetailPage = () => {
   const navigate = useNavigate();
   const { navigateToNFTDetail } = useExperience();
