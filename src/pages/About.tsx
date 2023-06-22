fe7fc1133975f7892863e8d4b28789bd5f496da6
Joe Sandmeyer <github.com@unconscious.com>
2023-06-21 21:24:23 -0400

    versions

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,7 +4,8 @@ import './Common.css'
 const AboutPage = () => {
   return (
 		<div>
-			<h1>Welcome to the About page</h1>
+			<h1>About page</h1>
+			<h3>v 0.0.001</h3>			
 			<Link to="/home"> Return to the Home page </Link>
 		</div>
   )
